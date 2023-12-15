f40d963cd7c6f3c20b1a4da85fd8b795ccd7d1ef
Khanbikov <153907547+Khanbikov@users.noreply.github.com>
2023-12-15 15:36:50 +0300

    Add files via upload

---
+++ task_5_5.py
@@ -0,0 +1,15 @@
+import json
+from pprint import pprint
+with open('template.json') as f:
+    file_content = f.read()
+    template = json.loads(file_content)
+
+
+pprint(template)
+model = template
+print("-------------------------------------------")
+
+for switch in model.keys():
+    if model[switch]['10/100Mbps'] == "5x" and \
+            model[switch]['MAC adress entries'] == "1K":
+        print(model[switch])
